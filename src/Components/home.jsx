19ae1afc7475a604d7a192a7d4d06068a95d8dbd
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-25 19:01:45 +0530

    add home

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import { Music_Context } from '../context'
-import { Header } from './index'
+import { DashBoardSongs, Header } from './index'
 function Home () {
   const data = useContext(Music_Context)
   console.log('DATA', data)
   return (
     <div className='w-screen h-screen bg-white'>
       <Header />
+      <DashBoardSongs />
     </div>
   )
 }
