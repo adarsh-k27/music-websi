932624c64fa67c35fd484ac2a9663e7e123f6d94
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-26 09:34:46 +0530

    music player

--- src/Components/DashboardSongs.jsx
+++ src/Components/DashboardSongs.jsx
@@ -5,30 +5,33 @@ import { AiOutlinePlus } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { Music_Context } from '../context/index'
 import { GetAllSongs } from '../apiHelper/song'
-import {MusicPlayer} from '../Components/index'
+import { MusicPlayer } from '../Components/index'
 import SongCard from './songCard'
-function DashboardSongs ({deleteBtn}) {
-  const {allSongs,Playing}=useContext(Music_Context).state
-  const {SetAllSongs}=useContext(Music_Context)
+function DashboardSongs ({ deleteBtn }) {
+  const { allSongs, Playing } = useContext(Music_Context).state
+  const { SetAllSongs } = useContext(Music_Context)
   const [isFocus, setFocus] = useState(true)
-  
-  useEffect(()=>{
 
-   GetAllSongs(SetAllSongs)
-   
-  },[])
+  useEffect(() => {
+    GetAllSongs(SetAllSongs)
+  }, [])
 
   const SongContainer = () => {
     return (
       <div className='w-full h-auto flex flex-col gap-2'>
         <p className='text-xs '>{allSongs?.length}</p>
         <div className='w-full h-auto flex flex-wrap gap-4 items-center justify-center'>
-          {
-            allSongs && allSongs.map((song,index)=>{
-              return <SongCard type={'song'} data={song} deleteBtn={deleteBtn} index={index} />
-
-            })
-          }
+          {allSongs &&
+            allSongs.map((song, index) => {
+              return (
+                <SongCard
+                  type={'song'}
+                  data={song}
+                  deleteBtn={deleteBtn}
+                  index={index}
+                />
+              )
+            })}
         </div>
       </div>
     )
@@ -36,11 +39,13 @@ function DashboardSongs ({deleteBtn}) {
   return (
     <div className='w-screen h-auto flex flex-col gap-6'>
       <div className='flex flex-row w-full py-2 gap-16 items-center justify-center'>
-        <div className='px-1 py-1 flex items-center justify-center hover:bg-white/50 hover:border-black/20 border-2 border-solid'>
-          <Link to={'/dashboard/new-song'}>
-            <AiOutlinePlus className='text-black/40 text-base ' />
-          </Link>
-        </div>
+        {deleteBtn && (
+          <div className='px-1 py-1 flex items-center justify-center hover:bg-white/50 hover:border-black/20 border-2 border-solid'>
+            <Link to={'/dashboard/new-song'}>
+              <AiOutlinePlus className='text-black/40 text-base ' />
+            </Link>
+          </div>
+        )}
 
         <div className='w-28 h-auto'>
           <input
@@ -51,8 +56,6 @@ function DashboardSongs ({deleteBtn}) {
         </div>
       </div>
       <SongContainer />
-      
-      
     </div>
   )
 }
