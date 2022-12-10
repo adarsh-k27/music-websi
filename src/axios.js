0d13a15d31728269c15fddef175bdab5b0ca6397
adarsh-k27 <adarshkdev27@gmail.com>
2022-12-10 16:14:27 +0530

    fix heroku issue

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "https://music-listener-app.onrender.com"
+    baseURL: "https://music-listener-app.onrender.com/"
 })
 
 export default axiosInstance;
